1cf40a253bdaa095f0ac355b02db2175f11b9aef
Rasmus <gitcommit@raz.sexy>
2016-01-26 09:42:48 +0100

    Initial commit

---
+++ src/main/se/molk/MathLib.java
@@ -0,0 +1,27 @@
+package se.molk;
+
+public final class MathLib {
+	public static double add(double x, double y) {
+		return 0;
+	}
+
+	public static double subtract(double x, double y) {
+		return 0;
+	}
+
+	public static double divide(double x, double y) {
+		return 0;
+	}
+
+	public static double multiply(double x, double y) {
+		return 0;
+	}
+
+	public static double squareRoot(double x) {
+		return 0;
+	}
+
+	public static double getPI() {
+		return 0;
+	}
+}
---
+++ src/test/se/molk/MathLibTests.java
@@ -0,0 +1,12 @@
+package se.molk;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MathLibTests {
+	@Test
+	public void testAddition() {
+		assertEquals(1, 0);
+	}
+}
